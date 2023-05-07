7a2adf6ce7d94ff5e39a533b51d6becd36c79637
AdamOrzech <adamorzech92@gmail.com>
2023-05-07 14:44:01 +0200

    After click on Back button it back me to the same page where I came and added preview of images before add to server

--- resources/views/articles/add.blade.php
+++ resources/views/articles/add.blade.php
@@ -1,42 +1,83 @@
-
 @extends('articles.layout')
 
 <div>
 
-<a href="{{"/articles"}}">
-    <button> @lang('common.back')</button>
-</a>
+    <a href="{{ URL::previous() }}"></i>
+        <button> @lang('common.back')</button>
+    </a>
 
-<div class="addMember-box">
-    <form action="{{route(('articles.create'))}}" method="POST" enctype="multipart/form-data">
-        <h1>@lang('add.add-articles')</h1>
+    <div class="addMember-box">
+        <form action="{{route(('articles.create'))}}" method="POST" enctype="multipart/form-data">
+            <h1>@lang('add.add-articles')</h1>
 
-        <p></p>
-        @csrf
-
-
-
-        <input type="text" name="title" placeholder=@lang('add.enter-title')>
+            <p></p>
+            @csrf
 
 
-        <textarea name="summary" placeholder=@lang('add.enter-summary')></textarea>
+            <input type="text" name="title" placeholder=@lang('add.enter-title')>
 
 
-        <textarea name="content" placeholder=@lang('add.enter-content')></textarea>
+            <textarea name="summary" placeholder=@lang('add.enter-summary')></textarea>
 
 
-
-        <button type="submit">@lang('add.add-new-article')</button>
-
+            <textarea name="content" placeholder=@lang('add.enter-content')></textarea>
 
 
-</div>
-    <div class="addPicture">
-        <input type="file" class="block shadow-5xl mb-10 p-2 w-80 italic placeholder-gray-400"
-               name="image">
+            <form method="post"  enctype="multipart/form-data">
+                @csrf
+                <div class="row">
+                    <div class="col-md-4"></div>
+                    <div class="form-group col-md-4">
+
+                        <form>
+                            <input type="file" id="image" name="image" accept="image/*" onchange="previewImage(event)">
+                        </form>
+
+                        <div id="imagePreview"></div>
+
+                        <script>
+                            function previewImage(event) {
+                                var reader = new FileReader();
+                                reader.onload = function () {
+                                    var imagePreview = document.getElementById('imagePreview');
+                                    var image = new Image();
+                                    image.src = reader.result;
+                                    image.onload = function () {
+                                        var canvas = document.createElement('canvas');
+                                        var ctx = canvas.getContext('2d');
+                                        var width = image.width;
+                                        var height = image.height;
+                                        var max_size = 500;
+                                        if (width > height) {
+                                            if (width > max_size) {
+                                                height *= max_size / width;
+                                                width = max_size;
+                                            }
+                                        } else {
+                                            if (height > max_size) {
+                                                width *= max_size / height;
+                                                height = max_size;
+                                            }
+                                        }
+                                        canvas.width = width;
+                                        canvas.height = height;
+                                        ctx.drawImage(image, 0, 0, width, height);
+                                        var dataUrl = canvas.toDataURL('image/jpeg');
+                                        imagePreview.innerHTML = '<img src="' + dataUrl + '" width="' + width + '" height="' + height + '">';
+                                    }
+                                }
+                                reader.readAsDataURL(event.target.files[0]);
+                            }
+                        </script>
+
+                        <button type="submit">@lang('add.add-new-article')</button>
+                    </div>
+
+
+            </form>
     </div>
-    </form>
 </div>
+
 
 
 
